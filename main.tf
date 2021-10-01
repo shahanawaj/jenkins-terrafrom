fdec46abde42717778af2f00ed5d363e00e529aa
Shaz <shahanawaj.patel@virtualclarity.com>
2021-10-01 16:15:33 +0530

    Initial commit

---
+++ main.tf
@@ -0,0 +1,34 @@
+
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-2.0*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  owners = ["amazon"] # Canonical
+}
+
+
+resource "aws_instance" "web" {
+  ami             = data.aws_ami.amazon_linux.id
+  instance_type   = "t2.micro"
+  key_name        = var.key_name
+  security_groups = [aws_security_group.jenkins_sg.name]
+  associate_public_ip_address = true
+  user_data       = "${file("install_jenkins.sh")}"
+  tags = {
+    Name = "Jenkins"
+  }
+}
---
+++ vars/terraform-tfvars.tf
@@ -0,0 +1,7 @@
+aws_region = "us-east-1"
+
+# vpc_id = "vpc-0a9ab196b78377f4f"
+
+cidr_block = "0.0.0.0/0"
+
+key_name = "mykey"
